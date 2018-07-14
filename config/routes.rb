ffa0f45bf77b1acef4e33869d922dafcbcb18217
Todd Johnson <toddjohnson407@gmail.com>
2018-07-14 14:45:28 -0600

    working on local server

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount ActionCable.server => '/cable'
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :chats, only: [:show, :index, :create] do
+   resources :messages, only: [:index, :create]
+  end
 end
